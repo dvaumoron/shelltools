03e354b97e3db29ac012bd3f3527867f41fa5250
dvaumoron <dvaumoron@gmail.com>
2023-08-14 13:34:18 +0200

    add a first version of linetojson

---
+++ linetojson/parser.go
@@ -0,0 +1,114 @@
+/*
+ *
+ * Copyright (C) 2023  Denis Vaumoron
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"slices"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	cmd := cobra.Command{
+		Use:   "linetojson COLUMNS [FILE]",
+		Short: "linetojson convert each line from FILE in a JSON object.",
+		Long:  "todo",
+		Args:  cobra.RangeArgs(1, 2),
+		RunE:  lineToJsonWithInit,
+	}
+
+	if err := cmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func lineToJsonWithInit(cmd *cobra.Command, args []string) error {
+	columns := spaceSplitter(args[0]) // TODO make this optional with autonaming of column
+
+	src := os.Stdin
+	if len(args) != 1 {
+		if filePath := args[1]; filePath != "-" {
+			src, err := os.Open(filePath)
+			if err != nil {
+				return err
+			}
+			defer src.Close()
+		}
+	}
+
+	sep := " " // TODO add an optional flag to change this default
+	splitter := spaceSplitter
+	if sep != " " {
+		splitter = func(rawValues string) []string {
+			return cleannedSplit(rawValues, sep)
+		}
+	}
+
+	skipLines := 0 // TODO add an optional flag to change this default
+	return lineToJson(columns, splitter, skipLines, src)
+}
+
+func spaceSplitter(rawValues string) []string {
+	splitted := strings.Split(rawValues, " ")
+	values := make([]string, 0, len(splitted))
+	for _, value := range splitted {
+		if value != "" {
+			values = append(values, value)
+		}
+	}
+	return slices.Clip(values)
+}
+
+func cleannedSplit(rawValues string, sep string) []string {
+	splitted := strings.Split(rawValues, sep)
+	for index, value := range splitted {
+		splitted[index] = strings.TrimSpace(value)
+	}
+	return slices.Clip(splitted)
+}
+
+func lineToJson(columns []string, splitter func(string) []string, skipLines int, src *os.File) error {
+	scanner := bufio.NewScanner(src)
+	for skipped := 0; skipped < skipLines; skipped++ {
+		scanner.Scan()
+	}
+
+	capacity := len(columns)
+	encoder := json.NewEncoder(os.Stdout)
+	for scanner.Scan() {
+		index := 0
+		current := make(map[string]string, capacity)
+		for _, value := range splitter(scanner.Text()) {
+			if value != "" {
+				current[columns[index]] = value
+				index++
+			}
+		}
+		if err := encoder.Encode(current); err != nil {
+			return err
+		}
+	}
+	return scanner.Err()
+}
